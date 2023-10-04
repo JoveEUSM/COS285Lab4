e84d6c82d325e2893a6a5584d2af2ae53dcb0abe
JoveEUSM <144271740+JoveEUSM@users.noreply.github.com>
2023-10-04 14:50:27 -0400

    Updated BinaryTree.java with javadoc
    Also changed node to node<Integer>

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,13 +1,21 @@
 public class BinaryTree {
 
-	protected node root;
-	
-	protected static class node<Integer>{
-		int data;
-		node left;
-		node right;
+	/*
+	 * The root node for this binary tree.
+	 * May have up to two children nodes, as with other binary tree nodes
+	 */
+	protected node<Integer> root;
+
+	//Nested node class for this binary tree
+	public static class node<Integer>{
+		//The data of this node. A simple int
+		private int data;
+		//The left child node of this node, if there is one.
+		private node<Integer> left;
+		//The right child node of this node, if there is one.
+		private node<Integer> right;
 		
-		public node(int data, node left, node right) {
+		public node(int data, node<Integer> left, node<Integer> right) {
 			this.data = data;
 			this.left = left;
 			this.right = right;
@@ -19,6 +27,10 @@ public class BinaryTree {
 		
 	}
 	
+	
+	/*
+	 * Method for testing the subtask methods. Left empty to be overriden later.
+	 */
 	protected void testing() {
 	}
 	
